e5dea5c3c58614565a8f191cc25ac11667b72f86
sofiiaml <sofiia.molotiuk.ik.2023@lpnu.ua>
2023-10-26 20:46:40 +0300

    lab05.4

---
+++ UnitTest05.4/UnitTest05.4.cpp
@@ -0,0 +1,19 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../PR05.4/PR05.4.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest054
+{
+	TEST_CLASS(UnitTest054)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			double result = S1(5, 2);
+			Assert::AreEqual(12.7267655113264423471543996693, result, 0.000000001);
+		}
+	};
+}
